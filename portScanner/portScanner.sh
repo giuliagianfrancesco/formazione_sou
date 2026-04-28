cdfef1393be75ee9ab102fe14493badcf66b5e06
giuliagianfrancesco <giulia.gianfrancesco@sourcesense.com>
2026-04-28 15:46:33 +0200

    Update portScanner.sh

--- portScanner/portScanner.sh
+++ portScanner/portScanner.sh
@@ -10,29 +10,30 @@ if [ $2 -lt 1024 ] || [ $3 -gt 49151 ]; then
 fi
 
 IFS='.' read -r -a ott <<< "$1"
-j=0
-while [ $j -le 3 ]
+numOtt=0
+while [ ${numOtt} -le 3 ]
 do
-if [ -z ${$ott[3]} ]; then
-echo "ip errato"
-exit 1;
-fi
+    if [ -z ${$ott[3]} ]; then
+        echo "ip errato"
+        exit 1;
+    fi
 
-if [ ${ott[j]} -gt 255 ] || [ ${ott[j]} -lt 0 ]; then
-echo "ip errato"
-exit 1;
-fi
+    if [ ${ott[j]} -gt 255 ] || [ ${ott[j]} -lt 0 ]; then
+        echo "ip errato"
+        exit 1;
+    fi
 
-let j=j+1
+    let numOtt=numOtt+1
 done
-i=$2
-touch file.txt
-while [ $i -le $3 ]
-do
-nc -v -w 1 $1 $i > file.txt 2>&1
-if grep -q "succeed" file.txt; then
-    echo "porta connessa $i"
 
-fi
-let i=i+1
+numPorta=$2
+touch file.txt
+
+while [ ${numPorta} -le $3 ]
+do
+    nc -v -w 1 $1 ${numPorta} > file.txt 2>&1
+    if grep -q "succeed" file.txt; then
+        echo "porta connessa ${numPorta}"
+    fi
+    let numPorta=numPorta+1
 done
