b8f56bdb14162c644611a03a1f38381ed01f7ac2
giuliagianfrancesco <giulia.gianfrancesco@sourcesense.com>
2026-04-30 12:30:39 +0200

    Add files via upload

---
+++ countProc/countProc.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+numPIDmax=$2
+numPIDmin=$1
+
+if [ "$2" -lt "0" ]; then
+   ((exitCode++))
+
+fi
+if [ "$1" -gt "$2" ]; then
+   let exitCode=exitCode+2
+
+fi
+declare -A count
+count[S]=0
+count[Z]=0
+count[R]=0
+count[D]=0
+count[T]=0
+count[I]=0
+
+checkStatus() { 
+s=$1
+if [ "$s" == "S" ]; then
+    let count[S]=count[S]+1
+
+fi
+if [ "$s" == "Z" ]; then
+    let count[Z]=count[Z]+1
+
+fi
+if [ "$s" == "D" ]; then
+    let count[D]=count[D]+1
+
+fi
+if [ "$s" == "R" ]; then
+    let count[R]=count[R]+1
+
+fi
+if [ "$s" == "T" ]; then
+    let count[T]=count[T]+1
+
+fi
+if [ "$s" == "I" ]; then
+    let count[I]=count[I]+1
+
+fi
+
+}
+
+findStatus() {
+    p=$1
+    ps u -p $p > file.txt
+    var=$(awk 'NR==2 {print $8}' file.txt)
+    var=${var:0:1}
+    echo $var
+}
+
+proc=$numPIDmin
+while [ "$proc" -le "$numPIDmax" ]
+do
+
+    status=$(findStatus $proc)
+    checkStatus $status
+    ((proc++))
+done
+echo "Processi negli stati: Sleeping, Zombie, Running, Uninterruptible, Stopped, idle"
+echo "S: ${count[S]} | Z: ${count[Z]} | R: ${count[R]} | D: ${count[D]} | T: ${count[T]}, | I: ${count[I]}"
+exit $exitCode
